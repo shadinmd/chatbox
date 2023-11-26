9e11fdcb74a467646a24bd1fb7a161718650d1b0
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2023-11-26 21:06:58 +0530

    added edit and userDetails usecase

--- backend/usecases/user.usecase.ts
+++ backend/usecases/user.usecase.ts
@@ -1,5 +1,56 @@
-class UserUsecase {
+import IUser from "../interface/user"
+import UserRepository from "../repository/user.repository"
 
+class UserUsecase {
+	private userRepository: UserRepository
+
+	constructor(userRepository: UserRepository) {
+		this.userRepository = userRepository
+	}
+
+	async userDetails(id: string) {
+		const response = await this.userRepository.findById(id)
+		try {
+			return {
+				status: response.success ? 200 : 500,
+				data: {
+					success: response.success,
+					message: response.message
+				}
+			}
+		} catch (error) {
+			console.log(error)
+			return {
+				status: 500,
+				data: {
+					success: false,
+					message: "server error"
+				}
+			}
+		}
+	}
+
+	async edit(user: IUser) {
+		try {
+			const response = await this.userRepository.update(user)
+			return {
+				status: response.success ? 200 : 500,
+				data: {
+					success: response.success,
+					message: response.message
+				}
+			}
+		} catch (error) {
+			console.log(error)
+			return {
+				status: 500,
+				data: {
+					success: false,
+					message: "server error"
+				}
+			}
+		}
+	}
 }
 
 export default UserUsecase
