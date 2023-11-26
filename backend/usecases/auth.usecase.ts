b2596111ef0059a91270a72a3380145d2994c93c
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2023-11-26 21:07:33 +0530

    added login and register usecase

--- backend/usecases/auth.usecase.ts
+++ backend/usecases/auth.usecase.ts
@@ -1,25 +1,74 @@
 import UserRepository from "../repository/user.repository";
+import JwtRepository from "../repository/jwt.repository"
+import BcryptRepository from "../repository/bcrypt.repository"
 
 class AuthUsecase {
 	private userRepository: UserRepository
+	private jwtRepository: JwtRepository
+	private bcryptRepostiroy: BcryptRepository
 
-	constructor(userRepository: UserRepository) {
+	constructor(userRepository: UserRepository, jwtRepository: JwtRepository, bcryptRepostiroy: BcryptRepository) {
 		this.userRepository = userRepository
+		this.jwtRepository = jwtRepository
+		this.bcryptRepostiroy = bcryptRepostiroy
 	}
 
-	async register() {
+	async register(id: string, email: string, username: string, password: string) {
 		try {
-
+			const response = await this.userRepository.create({ id, username, email, password })
+			return {
+				status: response.success ? 200 : 500,
+				data: {
+					success: response.success,
+					message: response.message
+				}
+			}
 		} catch (error) {
-
+			console.log(error)
+			return {
+				status: 500,
+				data: {
+					success: false,
+					message: "server error"
+				}
+			}
 		}
 	}
 
-	async login() {
+	async login(email: string, password: string) {
 		try {
+			const response = await this.userRepository.findByEmail(email)
+			const result = {
+				status: response.success ? 200 : 500,
+				data: {
+					success: response.success,
+					message: response.message,
+					token: ''
+				}
+			}
+			if (!response.success) {
+				return result
+			}
 
+			if (!this.bcryptRepostiroy.compare(password, response.user?.password!)) {
+				result.status = 500
+				result.data.success = false
+				result.data.message = "incorrect email or password"
+			}
+
+			const token = await this.jwtRepository.encode(response.user?.id)
+			result.data.token = token
+
+			return result
 		} catch (error) {
-
+			console.log(error)
+			return {
+				status: 500,
+				data: {
+					success: false,
+					message: "server error"
+				}
+			}
 		}
 	}
 }
