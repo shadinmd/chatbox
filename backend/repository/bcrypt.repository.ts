5754544dd1bc8aa25c99380aee977eee10e5f496
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2023-11-26 21:04:27 +0530

    added bcrypt repository

---
+++ backend/repository/bcrypt.repository.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt"
+
+class BcryptRepository {
+	constructor() { }
+
+	hash(payload: string) {
+		const saltRound = 10
+		const salt = bcrypt.genSaltSync(saltRound)
+		const hash = bcrypt.hashSync(payload, salt)
+		return hash
+	}
+
+	compare(data: string, hash: string) {
+		const result = bcrypt.compareSync(data, hash)
+		return result
+	}
+}
+
+export default BcryptRepository
