388f7a572da73633ba22bfa84186086003a55fb2
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2023-11-30 22:58:24 +0530

    admin and user route addition

--- backend/main.ts
+++ backend/main.ts
@@ -1,14 +1,19 @@
 import createServer from "./configs/server"
 import connectDb from "./configs/database"
+import { Request, Response } from "express"
 
 import authRoute from "./routes/auth.route"
-import { Request, Response } from "express"
+import userRoute from "./routes/user.route"
+import adminRoute from "./routes/admin.route"
+
 
 const app = createServer()
 connectDb()
 
 // routes
 app.use("/auth", authRoute)
+app.use("/user", userRoute)
+app.use("/admin", adminRoute)
 
 app.get("/", (req: Request, res: Response) => {
 	res.redirect("https://shadinmhd.in")
