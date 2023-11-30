17d70591d5a40b24f45f9418e31cde457a3fdb23
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2023-11-30 22:58:08 +0530

    added otp search and create

--- backend/repository/otp.repository.ts
+++ backend/repository/otp.repository.ts
@@ -18,7 +18,35 @@ class OtpRepository {
 			}
 		}
 	}
-	async searchByEmail(email: string) { }
+
+	async searchById(id: string) {
+		try {
+			const response = await OtpModel.findOne({ user: id })
+			return {
+				success: true,
+				message: "otp found",
+				otp: response
+			}
+		} catch (error) {
+			console.log(error)
+			return {
+				success: false,
+				message: "database error"
+			}
+		}
+	}
+
+	async searchByEmail(email: string) {
+		try {
+
+		} catch (error) {
+			console.log(error)
+			return {
+				success: false,
+				message: "database error"
+			}
+		}
+	}
 }
 
 export default OtpRepository
