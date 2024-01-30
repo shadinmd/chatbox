4317414de404ff1a63fd9927746cf0082737c8e2
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-01-30 13:25:39 +0530

    fixed: made bio optional

--- frontend/src/models/profileFormSchema.ts
+++ frontend/src/models/profileFormSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 const profileFormSchema = z.object({
 	username: z.string().min(3, { message: "username too short" }),
 	email: z.string().email("enter a valid email"),
-	bio: z.string().nullable(),
+	bio: z.string().optional(),
 })
 
 export default profileFormSchema
